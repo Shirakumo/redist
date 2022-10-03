fcad2d7bdeda7699853e274cf903573d02284411
Shinmera <shinmera@tymoon.eu>
2022-10-03 15:42:59 +0200

    Fix readme, allow keywords in source types

--- dist.lisp
+++ dist.lisp
@@ -180,6 +180,9 @@
 
 (defmethod ensure-source ((spec cons))
   (destructuring-bind (type url . initargs) spec
+    (when (typep type 'keyword)
+      (setf type (or (find-symbol (symbol-name type) #.*package*)
+                     (error "No source type with name ~s found!" type))))
     (apply #'make-instance type :url url initargs)))
 
 (defclass project ()
