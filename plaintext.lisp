534e0962c4a32f0a8b0cfd1352330015be21dbca
Shinmera <shinmera@tymoon.eu>
2024-07-27 09:04:56 +0200

    Completed the plaintext backend (untested)

--- plaintext.lisp
+++ plaintext.lisp
@@ -27,24 +27,24 @@
             (*print-right-margin* 80)
             (*print-readably* NIL))
         (format stream "~&;;;;; Distinfo compiled automatically")
-        (terpri stream) (pprint '(in-package #.(package-name *package*)) stream)
-        (terpri stream) (pprint '(setf (id-counter *storage*) ,(id-counter *storage*)))
-        (terpri stream) (pprint '(setf (dir *storage*) ,(dir *storage*)))
+        (terpri stream) (pprint `(in-package #.(package-name *package*)) stream)
+        (terpri stream) (pprint `(setf (id-counter *storage*) ,(id-counter *storage*)))
+        (terpri stream) (pprint `(setf (dir *storage*) ,(dir *storage*)))
         (terpri stream)))))
 
 (defmethod store :before ((*storage* plaintext) (object stored-object) (slot (eql T)))
   (unless (stored-p object)
     (setf (id object) (incf (id-counter *storage*)))))
 
-(defun plaintext-file (object &optional slot)
+(defun plaintext-file (type id &optional slot)
   (merge-pathnames
-   (make-pathname :name (princ-to-string (id object))
+   (make-pathname :name (princ-to-string id)
                   :type (when slot (string-downcase slot))
-                  :directory (list :relative (string-downcase (type-of object))))
+                  :directory (list :relative (string-downcase type)))
    (dir *storage*)))
 
 (defun store-plaintext (object &rest fields)
-  (let ((file (plaintext-file object)))
+  (let ((file (plaintext-file (type-of object) (id object))))
     (with-open-file (stream file :direction :output :if-exists :supersede)
       (with-standard-io-syntax
         (let ((*package* #.*package*)
@@ -57,7 +57,7 @@
           (format stream ")~%"))))))
 
 (defun store-slot (object slot value)
-  (let ((file (plaintext-file object slot)))
+  (let ((file (plaintext-file (type-of object) (id object) slot)))
     (with-open-file (stream file :direction :output :if-exists :supersede)
       (with-standard-io-syntax
         (let ((*package* #.*package*)
@@ -66,44 +66,61 @@
               (*print-readably* NIL))
           (format stream "~&~s~%" value))))))
 
+(defun read-plaintext (file)
+  (with-open-file (stream file :direction :input)
+    (let ((*package* #.*package*))
+      (read stream))))
+
 (defun retrieve-plaintext (object &optional slot)
-  (let ((file (plaintext-file object slot)))
-    (with-open-file (stream file :direction :input)
-      (let ((*package* #.*package*))
-        (read stream)))))
+  (read-plaintext (plaintext-file (type-of object) (id object) slot)))
+
+(defun retrieve-listed (object slot type existing)
+  (loop for id in (retrieve-plaintext object slot)
+        collect (apply #'ensure-instance (find id existing :key #'id) type
+                       (read-plaintext (plaintext-file type id)))))
 
 (defmethod retrieve ((*storage* plaintext) (object dist) (name string))
-  )
+  (let ((id (pathname-name (truename (plaintext-file 'dist name)))))
+    (ensure-instance (gethash name *dists*) 'dist :id id :name name)))
 
 (defmethod retrieve ((*storage* plaintext) (object (eql 'dist)) (all (eql T)))
-  )
+  (dolist (file (directory (plaintext-file 'dist "*")))
+    (when (every #'digit-char-p (pathname-name file))
+      (let* ((args (read-plaintext file))
+             (name (getf args :name)))
+        (setf (dist name) (apply #'ensure-instance (gethash name *dists*) 'dist args))))))
 
 (defmethod retrieve ((*storage* plaintext) (object dist) (slot (eql T)))
-  )
+  (apply #'reinitialize-instance object (retrieve-plaintext object)))
 
 (defmethod retrieve ((*storage* plaintext) (object dist) (slot (eql 'excluded-paths)))
   (setf (excluded-paths object) (retrieve-plaintext object slot)))
 
 (defmethod retrieve ((*storage* plaintext) (object dist) (slot (eql 'projects)))
-  )
+  (setf (projects object) (retrieve-listed object slot 'project (projects object))))
 
 (defmethod retrieve ((*storage* plaintext) (object dist) (slot (eql 'releases)))
-  )
+  (setf (releases object) (retrieve-listed object slot 'release (releases object))))
 
 (defmethod retrieve ((*storage* plaintext) (object release) (slot (eql T)))
-  )
+  (apply #'reinitialize-instance object (retrieve-plaintext object)))
 
 (defmethod retrieve ((*storage* plaintext) (object release) (slot (eql 'projects)))
-  )
+  (setf (projects object) (retrieve-listed object slot 'project (projects object))))
 
 (defmethod retrieve ((*storage* plaintext) (object project) (name string))
-  )
+  (let ((id (pathname-name (truename (plaintext-file 'project name)))))
+    (ensure-instance (gethash name *projects*) 'project :id id :name name)))
 
 (defmethod retrieve ((*storage* plaintext) (object (eql 'project)) (all (eql T)))
-  )
+  (dolist (file (directory (plaintext-file 'project "*")))
+    (when (every #'digit-char-p (pathname-name file))
+      (let* ((args (read-plaintext file))
+             (name (getf args :name)))
+        (setf (project name) (apply #'ensure-instance (gethash name *projects*) 'project args))))))
 
 (defmethod retrieve ((*storage* plaintext) (object project) (slot (eql T)))
-  )
+  (apply #'reinitialize-instance object (retrieve-plaintext object)))
 
 (defmethod retrieve ((*storage* plaintext) (object project) (slot (eql 'excluded-systems)))
   (setf (excluded-systems object) (retrieve-plaintext object slot)))
@@ -112,10 +129,10 @@
   (setf (excluded-paths object) (retrieve-plaintext object slot)))
 
 (defmethod retrieve ((*storage* plaintext) (object project) (slot (eql 'releases)))
-  )
+  (setf (releases object) (retrieve-listed object slot 'project-release (releases object))))
 
 (defmethod retrieve ((*storage* plaintext) (object project-release) (slot (eql T)))
-  )
+  (apply #'reinitialize-instance object (retrieve-plaintext object)))
 
 (defmethod retrieve ((*storage* plaintext) (object project-release) (slot (eql 'source-files)))
   (setf (source-files object) (retrieve-plaintext object slot)))
@@ -124,6 +141,9 @@
   (store-plaintext object
                    :name (name object)
                    :url (url object))
+  (let ((link (plaintext-file 'dist (name object))))
+    (when (probe-file link) (delete-file link))
+    (filesystem-utils:create-symbolic-link link (plaintext-file 'dist (id object))))
   (store *storage* object 'excluded-paths)
   (store *storage* object 'projects)
   (store *storage* object 'releases))
@@ -157,6 +177,9 @@
                    :source-directory (relpath (source-directory object) (default-source-directory))
                    :disabled-p (disabled-p object)
                    :sources (mapcar #'serialize (sources object)))
+  (let ((link (plaintext-file 'project (name object))))
+    (when (probe-file link) (delete-file link))
+    (filesystem-utils:create-symbolic-link link (plaintext-file 'project (id object))))
   (store *storage* object 'excluded-systems)
   (store *storage* object 'excluded-paths)
   (store *storage* object 'releases))
